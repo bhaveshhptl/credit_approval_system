b971000322c34bd86fb3a9a72c2d20d819674cc4
Bhavesh Patil2 <patilbhavesh1223@gmail.com>
2025-06-03 20:45:44 +0530

    Updated docker configuration for running in a single command

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import django
+from api.models import Customer, Loan
 from api.tasks import load_customer_data, load_loan_data
 
 sys.path.append('/app')
@@ -10,12 +11,13 @@ django.setup()
 
 
 if __name__ == '__main__':
-    print("Loading customer data...")
-    result1 = load_customer_data()
-    print(result1)
-    
-    print("Loading loan data...")
-    result2 = load_loan_data()
-    print(result2)
-    
-    print("Data loading completed!")
+    if Customer.objects.exists() and Loan.objects.exists():
+        print("Customers already loaded, skipping ingestion.")
+    else:
+        print("Loading customer data...")
+        result1 = load_customer_data()
+        print(result1)
+        print("Loading loan data...")
+        result2 = load_loan_data()
+        print(result2)
+        print("Data loading completed!")
